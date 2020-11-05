eafc1cfdcfea1adb6cec202d531cda53807a4aba
wil-bert <ianwilbuts@gmail.com>
2020-11-05 11:57:24 +0300

    add API key

---
+++ app/src/main/java/com/moringaschool/workoutapp/Constants.java
@@ -0,0 +1,6 @@
+package com.moringaschool.workoutapp;
+
+public class Constants {
+    public static final String WGER_BASE_URL = "https://wger.de/api/v2/";
+    public static final String WGER_API_KEY = BuildConfig.BUILD_TYPE;
+}
